45d8e39bb78af05dc5e8540c759c2fc223995280
diegocaettano <diegocaettano@live.com>
2020-07-29 14:02:38 -0300

    Aula 2

---
+++ src/pages/cadastro/Video/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function CadastroVideo(){
+    return(
+      <div>
+        Página de Cadstro de Videos
+      </div>
+    )
+  }
+
+  export default CadastroVideo;
